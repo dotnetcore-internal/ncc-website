fd213559442b916ba1ee799973351902ebae7b88
alexinea <forerunner@outlook.com>
2019-03-11 14:18:11 +0800

    Add WTM and CanalSharp

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# Source code of projects.dotnet-china.org
+# .NET Core Community 社区成员与技术爱好者共建网站
 
+本站由 [.NET Core Community (NCC)](https://www.dotnetcore.xyz) 社区成员和技术爱好者共建，源码位于 GitHub。
 
+## 贡献
 
-please view [http://projects.dotnet-china.org](http://projects.dotnet-china.org)
+查看[贡献和使用说明](CONTRIBUTING.md)
--- config/swigOptions.js
+++ config/swigOptions.js
@@ -1,20 +1,18 @@
 module.exports = {
   data: {
-    title: 'dotnetChina',
+    title: "Home | .NET Core Community",
     css: {
-      root :  '/css/',
-      path :  '/css/pages/', //具体页面样式表路径
-      common: '/css/common.css'
+      root: "/css/",
+      path: "/css/pages/", //具体页面样式表路径
+      common: "/css/common.css"
     },
-    js:{
-      path: '/js/pages/',   //具体页面样式表
-      base: '/js/common.js',
-      libPath : "/js/lib/",
-      lib :{
-       
-      }
+    js: {
+      path: "/js/pages/", //具体页面样式表
+      base: "/js/common.js",
+      libPath: "/js/lib/",
+      lib: {}
     },
-    image:"/images/"
+    image: "/images/"
   },
-  defaults: {cache: false},
-}
+  defaults: { cache: false }
+};
