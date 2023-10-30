fe1eb24d3dd4f5435972c17188b5de04cbed4782
AlexLEWIS <forerunner@outlook.com>
2023-10-30 22:28:05 +0800

    Add OpenNCC View

---
+++ app/src/views/about/OpenNccView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  <div class="responsive-width text-2xl text-black dark:text-white">
+    <div class="text-3xl font-black p-5">NCC WECHAT</div>
+    <div class="text-2xl p-5">Connect with us on WeChat by scanning this QR code!</div>
+    <div>
+      <img src="/images/official-wechat-account-qrcode.jpg" alt="NCC WeChat QR Code" width="500" class=""/>
+    </div>
+  </div>
+
+</template>
+
+<style scoped lang="css">
+
+</style>
