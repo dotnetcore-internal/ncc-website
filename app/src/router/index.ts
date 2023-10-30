de1d3feb702cc5a7deeead247592beb72719be80
AlexLEWIS <forerunner@outlook.com>
2023-10-30 22:27:53 +0800

    Update site router

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,14 +1,29 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/:pathMatch(.*)*",
-      name: "all",
-      component: () => import("../views/Home.vue"),
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: () => import("../views/Home.vue"),
+        },
+        {
+            path: "/all-ncc",
+            name: "all-ncc",
+            component: () => import("../views/about/AllNccView.vue"),
+        },
+        {
+            path: "/wechat-open-ncc",
+            name: "wechat-open-ncc",
+            component: () => import("../views/about/OpenNccView.vue"),
+        },
+        {
+            path: "/:pathMatch(.*)*",
+            name: "all",
+            component: () => import("../views/404.vue"),
+        }
+    ]
 });
 
 export default router;
---
+++ app/src/views/about/AllNccView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  <div class="responsive-width text-2xl text-black dark:text-white min-h-75vh">All NCC Sites</div>
+
+</template>
+
+<style scoped lang="css">
+
+</style>
