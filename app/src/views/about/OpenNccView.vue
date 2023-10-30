7a9f5d162f7a6d3f917ead6efc5c5140dca99f46
AlexLEWIS <forerunner@outlook.com>
2023-10-31 00:09:11 +0800

    Add mouse action for WeChat QR for footer.

--- app/src/views/about/OpenNccView.vue
+++ app/src/views/about/OpenNccView.vue
@@ -8,7 +8,7 @@
     <div class="text-3xl font-black p-5">NCC WECHAT</div>
     <div class="text-2xl p-5">Connect with us on WeChat by scanning this QR code!</div>
     <div>
-      <img src="/images/official-wechat-account-qrcode.jpg" alt="NCC WeChat QR Code" width="500" class=""/>
+      <img src="/images/official-wechat-account-qrcode.jpg" alt="NCC WeChat QR Code" width="500"/>
     </div>
   </div>
 
