076c952f58fdfcd7be939f1f3c38d3f687239405
AlexLEWIS <forerunner@outlook.com>
2023-10-31 21:25:37 +0800

    Add "Our projects", "Our Partners & Sponsors" and nav resource area.

--- app/src/App.vue
+++ app/src/App.vue
@@ -20,6 +20,107 @@ const {isOutside} = useMouseInElement(qrCodeBoxRef)
   <footer class="bg-black text-white">
     <div class="responsive-width">
 
+      <div class="h-5"></div>
+
+      <div class="p-5 grid grid-cols-6 gap-4">
+        <div class="nav-about-us">
+          <span class="nav-title text-xs font-black pb-6 block">About Us</span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="/about/who-we-are" title="Who We Are">Who We Are</a>
+          </span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="/people/project-management-committee" title="Project Management Committee">Project Management Committee</a>
+          </span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="/conduct" title="Code of Conduct">Code of Conduct</a>
+          </span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="/about/story" title="Story">Story</a>
+          </span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="/about/faq" title="FAQ">FAQ</a>
+          </span>
+          <hr class="my-5 w-5 border-white/50" />
+          <span class="nav-item text-xs leading-7 block">
+            <a href="/about/contact" title="Contact Us">Contact Us</a>
+          </span>
+        </div>
+        <div class="nav-projects">
+          <span class="nav-title text-xs font-black pb-6 block">Projects</span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="https://contribute.ncc.work" title="Contribute">Contribute</a>
+          </span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="https://landscape.ncc.work" title="Landscape">Landscape</a>
+          </span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="https://tools.ncc.work" title="Project Tools">Project Tools</a>
+          </span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="/projects" title="Projects">Projects</a>
+          </span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="/archived-projects" title="Archived Projects">Archived Projects</a>
+          </span>
+          <hr class="my-5 w-5 border-white/50" />
+          <span class="nav-item text-xs leading-7 block">
+            <a href="https://contribute.ncc.work/project-join-ncc" title="How to Join Us">How to Join Us</a>
+          </span>
+        </div>
+        <div class="nav-Learning">
+          <span class="nav-title text-xs font-black pb-6 block">Learning</span>
+        </div>
+        <div class="nav-community">
+          <span class="nav-title text-xs font-black pb-6 block">Community</span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="https://events.ncc.work" title="Events">Events</a>
+          </span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="/meetup" title="Meetup">Meetup</a>
+          </span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="https://2f.ncc.work" title="NCC 2F">NCC 2F</a>
+          </span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="/community-online" title="Community Online">Community Online</a>
+          </span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="https://glossary.ncc.work" title="Glossary">Glossary</a>
+          </span>
+          <hr class="my-5 w-5 border-white/50" />
+          <span class="nav-item text-xs leading-7 block">
+            <a href="/participation-guidelines" title="Participation Guidelines">Participation Guidelines</a>
+          </span>
+        </div>
+        <div class="nav-resource">
+          <span class="nav-title text-xs font-black pb-6 block">Resource & Media</span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="/news" title="News">News</a>
+          </span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="/announcements" title="Announcements">Announcements</a>
+          </span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="/brand-guidelines" title="Brand Guidelines">Brand Guidelines</a>
+          </span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="https://github.com/dotnetcore/ncc-artwork" title="Project Logos">Project Logos</a>
+          </span>
+          <hr class="my-5 w-5 border-white/50" />
+          <span class="nav-item text-xs leading-7 block">
+            <a href="https://github.com/dotnetcore" title="NCC@GitHub">NCC@GitHub</a>
+          </span>
+          <span class="nav-item text-xs leading-7 block">
+            <a href="https://gitee.com/dotnetcore" title="NCC@Gitee">NCC@Gitee</a>
+          </span>
+        </div>
+        <div class="nav-resource">
+          Join Us
+        </div>
+      </div>
+
+      <div class="h-5"></div>
+
       <div class="p-5">
         <div class="inline-block align-middle">
           <a href="/" title=".NET Core Community">
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -6,9 +6,130 @@
 
   <div class="responsive-width text-2xl text-black dark:text-white min-h-75vh">Home</div>
 
+  <div class="">
+    <div class="responsive-width">
+      <div class="text-3xl dark:text-white tracking-tight p-5 pb-0">Our Projects</div>
+
+      <hr class="m-5 border-black dark:border-white"/>
+
+      <div class="text-lg font-light px-5 pb-10 sm:columns-4 lg:columns-5 columns-3">
+        <div class="project-letter">A</div>
+        <div class="project-name">
+          <a href="/projects/agileconfig" title="AgileConfig">AgileConfig</a>
+        </div>
+        <div class="project-name">
+          <a href="/projects/alipay-sdk" title="Alipay SDK">Alipay SDK</a>
+        </div>
+        <div class="project-name">
+          <a href="/projects/aspectcore" title="AspectCore">AspectCore</a>
+        </div>
+        <div class="project-letter">B</div>
+        <div class="project-name">
+          <a href="/projects/bootstrapblazor" title="BootstrapBlazor">BootstrapBlazor</a>
+        </div>
+        <div class="project-letter">C</div>
+        <div class="project-name">
+          <a href="/projects/canalsharp" title="CanelSharp">CanalSharp</a>
+        </div>
+        <div class="project-name">
+          <a href="/projects/cap" title="CAP">CAP</a>
+        </div>
+        <div class="project-name">
+          <a href="/projects/collection" title="Collection">Collection</a>
+        </div>
+        <div class="project-name">
+          <a href="/projects/compile-environment" title="Compile Environment">Compile Environment</a>
+        </div>
+        <div class="project-letter">D</div>
+        <div class="project-name">
+          <a href="/projects/dotnetspider" title="DotnetSpider">DotnetSpider</a>
+        </div>
+        <div class="project-letter">E</div>
+        <div class="project-name">
+          <a href="/projects/easycaching" title="EasyCaching">EasyCaching</a>
+        </div>
+        <div class="project-name">
+          <a href="/projects/efcore-gaussdb" title="EfCore GaussDB">EfCore GaussDB</a>
+        </div>
+        <div class="project-name">
+          <a href="/projects/efcore-kingbasees" title="EfCore KingbaseES">EfCore KingbaseES</a>
+        </div>
+        <div class="project-letter">F</div>
+        <div class="project-name">
+          <a href="/projects/fastgithub" title="FastGithub">FastGithub</a>
+        </div>
+        <div class="project-name">
+          <a href="/projects/flubucore" title="FlubuCore">FlubuCore</a>
+        </div>
+        <div class="project-name">
+          <a href="/projects/freesql" title="FreeSql">FreeSql</a>
+        </div>
+        <div class="project-letter">H</div>
+        <div class="project-name">
+          <a href="/projects/httpreports" title="HttpReports">HttpReports</a>
+        </div>
+        <div class="project-letter">K</div>
+        <div class="project-name">
+          <a href="/projects/kooboo-json" title="KoobooJson">KoobooJson</a>
+        </div>
+        <div class="project-letter">M</div>
+        <div class="project-name">
+          <a href="/projects/magicodes-ie" title="Magicodes Exporter & Importer">Magicodes IE</a>
+        </div>
+        <div class="project-name">
+          <a href="/projects/mocha" title="Mocha">Mocha</a>
+        </div>
+        <div class="project-letter">N</div>
+        <div class="project-name">
+          <a href="/projects/natasha" title="Natasha">Natasha</a>
+        </div>
+        <div class="project-name">
+          <a href="/projects/npoi" title="NPOI">NPOI</a>
+        </div>
+        <div class="project-letter">O</div>
+        <div class="project-name">
+          <a href="/projects/osharp" title="OSharp">OSharp</a>
+        </div>
+        <div class="project-letter">S</div>
+        <div class="project-name">
+          <a href="/projects/smartsql" title="SmartSql">SmartSql</a>
+        </div>
+        <div class="project-name">
+          <a href="/projects/smartcode" title="SmartCode">SmartCode</a>
+        </div>
+        <div class="project-letter">U</div>
+        <div class="project-name">
+          <a href="/projects/util" title="Util">Util</a>
+        </div>
+        <div class="project-letter">W</div>
+        <div class="project-name">
+          <a href="/projects/webapiclient" title="WebApiClient">WebApiClient</a>
+        </div>
+        <div class="project-name">
+          <a href="/projects/wtm" title="WTM">WTM</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="bg-gray-100 dark:bg-gray-900/50 dark:text-white">
+    <div class="responsive-width">
+      <div class="text-3xl tracking-tight p-5 pb-0">Our Partners & Sponsors</div>
+
+      <hr class="m-5 border-black dark:border-white"/>
+
+      <div class="p-5"> Content </div>
+
+    </div>
+
+  </div>
+
   <div class="bg-gray-900 text-white">
     <div class="responsive-width">
-      <div class="text-3xl p-5">Our Slogan</div>
+      <div class="text-3xl tracking-tight p-5 pb-0">Our Slogan</div>
+
+      <hr class="m-5 border-white"/>
+
       <div class="lg:text-3xl md:text-2xl sm:text-lg font-light italic px-5 pb-10">
         Gather the might of our age, merge the triumphs of the multitude, <br/>
         surmount hurdles and trials, unite aboard one vessel, and lo, <br/>
@@ -20,5 +141,18 @@
 </template>
 
 <style scoped lang="css">
+.project-letter{
+  @apply bg-gray-100 dark:bg-gray-800 w-1/6 m-0.5 rounded-2xl ;
+  @apply text-sm text-gray-600 dark:text-gray-200 text-center font-extrabold leading-8;
+}
 
+.project-name{
+  @apply dark:text-white leading-8;
+  font-size: 1rem;
+}
+
+.project-name:hover{
+  @apply underline underline-offset-4;
+  @apply text-purple-700 dark:text-purple-100;
+}
 </style>
