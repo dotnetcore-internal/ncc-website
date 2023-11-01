6ce72b3002a3250d96d4aa8d4dda8c50f364891a
AlexLEWIS <forerunner@outlook.com>
2023-11-01 11:31:48 +0800

    Update “Our Partners & Sponsors"

--- app/src/components/common/PartnersAndSponsors.vue
+++ app/src/components/common/PartnersAndSponsors.vue
@@ -10,9 +10,24 @@ import PlaceholderBlock from "@/components/blocks/PlaceholderBlock.vue";
 
       <title-block h1-mode="false" color-mode="auto" :with-horizontal-rule="true" :is-font-black="false">Our Partners & Sponsors</title-block>
 
-      <div class="p-5"> Content</div>
+      <a href="https://www.jetbrains.com/?from=.NETCoreCommunity(NCC)" target="_blank" title="JetBrains" class="partner-item">
+        <img src="/images/partners/jetbrains.svg" alt="JetBrains" class="w-full h-full">
+      </a>
+
+      <a href="https://www.myget.org/" target="_blank" title="MyGet" class="partner-item">
+        <img src="/images/partners/myget.svg" alt="MyGet" class="w-full h-full">
+      </a>
+
+      <a href="https://www.oreilly.com/pub/cpc/323206" target="_blank" title="MyGet" class="partner-item">
+        <img src="/images/partners/oreilly.png" alt="O'REILLY" class="w-full h-full">
+      </a>
+
+      <a href="https://www.material-theme.com/" target="_blank" title="MyGet" class="partner-item">
+        <img src="/images/partners/material-theme.png" alt="Material Theme" class="w-full h-full">
+      </a>
 
       <placeholder-block height="1.25rem"/>
+      
     </div>
 
   </div>
@@ -20,5 +35,9 @@ import PlaceholderBlock from "@/components/blocks/PlaceholderBlock.vue";
 </template>
 
 <style scoped lang="css">
+
+.partner-item {
+  @apply w-20 h-20 m-5 inline-block;
+}
 
 </style>
