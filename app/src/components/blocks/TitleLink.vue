267d494947e6b7cde112e190c89844a0d250d271
AlexLEWIS <forerunner@outlook.com>
2023-11-05 16:47:57 +0800

    Add title-link block

---
+++ app/src/components/blocks/TitleLink.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import Anchor from "@/components/basic/AnchorElement.vue";
+
+const props = withDefaults(defineProps<{
+  mode?: 'h5' | 'classic'
+  title?: string;
+  href?: string;
+  target?: '_blank' | '_self' | '_parent' | '_top';
+  routeName?: string;
+  classValue?: string;
+}>(), {
+  mode: 'h5',
+  title: '',
+  href: '#',
+  target: '_self'
+});
+</script>
+
+<template>
+  <div class="m-7 mt-9 hover:text-purple-500 font-bold border-b-4 border-purple-500">
+    <anchor :href="href" :route-name="routeName" :title="title" :mode="mode" class="leading-7" :class-value="classValue" :target="target">
+      <slot></slot>
+    </anchor>
+  </div>
+</template>
+
+<style scoped lang="css">
+
+</style>
