786bace14ac4e865a559bf0924d33a0438af46fa
AlexLEWIS <forerunner@outlook.com>
2023-11-07 12:05:10 +0800

    refactor locale switch comment

--- app/src/components/common/footer/FooterLocale.vue
+++ app/src/components/common/footer/FooterLocale.vue
@@ -2,6 +2,7 @@
 import {inject, ref} from "vue";
 import {onClickOutside} from "@vueuse/core";
 import {useRoute} from "vue-router";
+import {useEmitter} from "@/hooks/useEmitter";
 import {useUiStore} from "@/stores/uiStore";
 
 import {Earth} from "@icon-park/vue-next";
@@ -13,6 +14,7 @@ import TitleBlock from "@/components/blocks/TitleBlock.vue";
 const i18n = inject('$i18n');
 const route = useRoute();
 const uiStore = useUiStore();
+const emitter = useEmitter();
 
 const target = ref(null);
 const displayLanguagesBox = ref(false);
@@ -35,13 +37,35 @@ const setLanguage = async (locale: string) => {
 
   uiStore.setLocale(locale);
 
+  emitter.emit("toChangeLocale", {locale: locale});
+
   setLocaleToCookie(locale);
 }
 </script>
 
 <template>
 
-  <div class="p-5 py-7 pr-0 mx-auto align-middle float-right cursor-pointer">
+  <div class="p-5 py-7 pr-0 mx-auto align-middle float-right cursor-pointer relative">
+
+    <transition enter-active-class="animate__animated animate__fadeIn animate__faster"
+                leave-active-class="animate__animated animate__fadeOut animate__faster">
+
+      <div ref="target" class="languages-box" v-show="displayLanguagesBox">
+
+        <title-block h1-mode="false" color-mode="light" :with-horizontal-rule="false" :with-placeholder="true" :is-font-black="false">
+          <div class="text-lg text-center">{{ $t('i18n-switch-title') }}</div>
+        </title-block>
+
+        <div class="columns-1">
+          <span class="languages-item" @click="setLanguage('zh')">中文（简体）</span>
+          <span class="languages-item" @click="setLanguage('zh-tw')">中文（正体）</span>
+          <span class="languages-item" @click="setLanguage('en')">English</span>
+        </div>
+
+      </div>
+
+    </transition>
+
     <earth theme="outline" size="24" fill="#f8f8f8" :strokeWidth="2" @click="switchLanguagesBox" :title="$t('i18n-switch-tip')"/>
   </div>
 
@@ -49,38 +73,21 @@ const setLanguage = async (locale: string) => {
 
   <div class="fixed left-0 top-0 right-0 bottom-0 z-40" v-show="displayLanguagesBox"></div>
 
-  <transition enter-active-class="animate__animated animate__fadeIn animate__faster"
-              leave-active-class="animate__animated animate__fadeOut animate__faster">
 
-    <div ref="target" class="languages-box" v-show="displayLanguagesBox">
-
-      <title-block h1-mode="false" color-mode="light" :with-horizontal-rule="true" :is-font-black="false">
-        <div class="text-2xl text-center">{{ $t('i18n-switch-title') }}</div>
-      </title-block>
-
-      <div class="columns-1 mx-auto">
-        <span class="languages-item" @click="setLanguage('zh')">中文（简体）</span>
-        <span class="languages-item" @click="setLanguage('zh-tw')">中文（繁体）</span>
-        <span class="languages-item" @click="setLanguage('en')">English</span>
-      </div>
-
-    </div>
-
-  </transition>
 
 </template>
 
 <style scoped lang="css">
 .languages-box {
-  @apply fixed left-36 top-36 right-36 bottom-36 p-5 z-50;
-  @apply bg-white/60 text-black;
-  @apply backdrop-blur-md rounded-2xl shadow-2xl;
+  @apply absolute -right-16 bottom-14 pb-5 w-52 z-50 overflow-hidden;
+  @apply bg-white text-black;
+  @apply rounded-2xl shadow-2xl;
 }
 
 .languages-item {
-  @apply p-1 block m-3;
+  @apply p-1 px-3 block;
   @apply text-center;
-  @apply hover:bg-purple-200/30;
+  @apply hover:bg-purple-400/20;
   @apply cursor-pointer;
 }
 </style>
