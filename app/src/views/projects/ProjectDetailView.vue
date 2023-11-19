c1bc4a54f2e20abbf94e0b34089b3f79e0a745c0
AlexLEWIS <forerunner@outlook.com>
2023-11-19 15:50:34 +0800

    Code cleanup

--- app/src/views/projects/ProjectDetailView.vue
+++ app/src/views/projects/ProjectDetailView.vue
@@ -275,15 +275,15 @@ onUnmounted(() => {
 
           <!-- Project Status Card -->
           <span v-if="useProject?.status==='archived'" class="tip archived">
-        <anchor href="/archived-projects" :title="$t('project-archived')" mode="classic">
-           {{ $t("project-archived") }}
-        </anchor>
-      </span>
+            <anchor href="/archived-projects" :title="$t('project-archived')" mode="classic">
+               {{ $t("project-archived") }}
+            </anchor>
+          </span>
           <span v-else-if="useProject?.status==='top-level'" class="tip toplevel">
-        <anchor href="/top-level-projects" :title="$t('project-top-level')" mode="classic">
-           {{ $t("project-top-level") }}
-        </anchor>
-      </span>
+            <anchor href="/top-level-projects" :title="$t('project-top-level')" mode="classic">
+               {{ $t("project-top-level") }}
+            </anchor>
+          </span>
           <span v-else-if="useProject?.status==='sandbox'" class="tip sandbox">{{ $t("project-sandbox") }}</span>
           <span v-else-if="useProject?.status==='incubation'" class="tip incubation">{{ $t("project-incubation") }}</span>
           <span v-else-if="useProject?.status==='labs'" class="tip labs">{{ $t("project-laboratory") }}</span>
@@ -297,34 +297,34 @@ onUnmounted(() => {
 
           <!-- Project GitHub Card -->
           <span v-if="displayGitHubSource" class="tip2 align-middle">
-        <anchor :href="useProject?.github" title="GitHub" target="_blank" mode="classic">
-             <github class="inline-block align-middle" theme="filled" size="14" :fill="useIconColor" />
-              <span class="inline-block align-middle ml-1 font-medium">GitHub</span>
-        </anchor>
-      </span>
+            <anchor :href="useProject?.github" title="GitHub" target="_blank" mode="classic">
+                 <github class="inline-block align-middle" theme="filled" size="14" :fill="useIconColor" />
+                  <span class="inline-block align-middle ml-1 font-medium">GitHub</span>
+            </anchor>
+          </span>
 
           <!-- Project Gitee Card -->
           <span v-if="displayGiteeSource" class="tip2 align-middle">
-        <anchor :href="useProject?.gitee" title="Gitee" target="_blank" mode="classic">
-              <svg class="inline-block align-middle icon"
-                   t="1698644520030" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
-                   p-id="4566" data-spm-anchor-id="a313x.search_index.0.i5.73f13a81QDVP27"
-                   :width="14" :height="14">
-                <path
-                  d="M625.727964 666.847792q9.151997 9.151997 0 17.727994-35.423989 35.423989-113.727964 35.423989t-113.727964-35.423989q-9.151997-8.575997 0-17.727994 3.423999-3.423999 8.575997-3.423999t8.575997 3.423999q27.423991 27.999991 96.57597 27.999991 68.575979 0 96.57597-27.999991 3.423999-3.423999 8.575997-3.423999t8.575997 3.423999zM450.272019 563.423824q0 21.151993-14.847995 35.999989t-35.999989 14.847995-36.287989-14.847995-15.135995-35.999989q0-21.727993 15.135995-36.575989t36.287989-14.847995 35.999989 15.135995 14.847995 36.287989zM675.999949 563.423824q0 21.151993-15.135995 35.999989t-36.287989 14.847995-35.999989-14.847995-14.847995-35.999989 14.847995-36.287989 35.999989-15.135995 36.287989 14.847995 15.135995 36.575989zM819.423904 494.847845q0-27.999991-19.999994-47.999985t-48.575985-19.999994-49.151985 20.575994q-74.271977-51.423984-177.727944-54.847983l35.999989-161.727949 114.271964 25.727992q0 21.151993 14.847995 35.999989t35.999989 14.847995 36.287989-15.135995 15.135995-36.287989-15.135995-36.287989-36.287989-15.135995q-30.84799 0-45.727986 28.575991l-126.271961-27.999991q-10.847997-2.847999-14.271996 9.151997l-39.423988 178.271944q-102.847968 3.999999-176.575945 55.423983-19.999994-21.151993-49.727984-21.151993-28.575991 0-48.575985 19.999994t-19.999994 47.999985q0 19.999994 10.559997 36.575989t28.287991 25.151992q-3.423999 15.423995-3.423999 31.99999 0 81.151975 79.999975 138.847957t192.57594 57.727982q113.151965 0 193.15194-57.727982t79.999975-138.847957q0-18.271994-3.999999-32.57599 17.151995-8.575997 27.423991-24.863992t10.271997-36.287989zM1023.99984 511.99984q0 103.999968-40.575987 198.847938t-109.151966 163.423949-163.423949 109.151966-198.847938 40.575987-198.847938-40.575987-163.423949-109.151966-109.151966-163.423949-40.575987-198.847938 40.575987-198.847938 109.151966-163.423949 163.423949-109.151966 198.847938-40.575987 198.847938 40.575987 163.423949 109.151966 109.151966 163.423949 40.575987 198.847938z"
-                  p-id="4567" data-spm-anchor-id="a313x.search_index.0.i4.73f13a81QDVP27" class="selected" :fill="useIconColor"></path>
-              </svg>
-              <span class="inline-block align-middle ml-1 font-medium">Gitee</span>
-        </anchor>
-      </span>
+            <anchor :href="useProject?.gitee" title="Gitee" target="_blank" mode="classic">
+                  <svg class="inline-block align-middle icon"
+                       t="1698644520030" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                       p-id="4566" data-spm-anchor-id="a313x.search_index.0.i5.73f13a81QDVP27"
+                       :width="14" :height="14">
+                    <path
+                      d="M625.727964 666.847792q9.151997 9.151997 0 17.727994-35.423989 35.423989-113.727964 35.423989t-113.727964-35.423989q-9.151997-8.575997 0-17.727994 3.423999-3.423999 8.575997-3.423999t8.575997 3.423999q27.423991 27.999991 96.57597 27.999991 68.575979 0 96.57597-27.999991 3.423999-3.423999 8.575997-3.423999t8.575997 3.423999zM450.272019 563.423824q0 21.151993-14.847995 35.999989t-35.999989 14.847995-36.287989-14.847995-15.135995-35.999989q0-21.727993 15.135995-36.575989t36.287989-14.847995 35.999989 15.135995 14.847995 36.287989zM675.999949 563.423824q0 21.151993-15.135995 35.999989t-36.287989 14.847995-35.999989-14.847995-14.847995-35.999989 14.847995-36.287989 35.999989-15.135995 36.287989 14.847995 15.135995 36.575989zM819.423904 494.847845q0-27.999991-19.999994-47.999985t-48.575985-19.999994-49.151985 20.575994q-74.271977-51.423984-177.727944-54.847983l35.999989-161.727949 114.271964 25.727992q0 21.151993 14.847995 35.999989t35.999989 14.847995 36.287989-15.135995 15.135995-36.287989-15.135995-36.287989-36.287989-15.135995q-30.84799 0-45.727986 28.575991l-126.271961-27.999991q-10.847997-2.847999-14.271996 9.151997l-39.423988 178.271944q-102.847968 3.999999-176.575945 55.423983-19.999994-21.151993-49.727984-21.151993-28.575991 0-48.575985 19.999994t-19.999994 47.999985q0 19.999994 10.559997 36.575989t28.287991 25.151992q-3.423999 15.423995-3.423999 31.99999 0 81.151975 79.999975 138.847957t192.57594 57.727982q113.151965 0 193.15194-57.727982t79.999975-138.847957q0-18.271994-3.999999-32.57599 17.151995-8.575997 27.423991-24.863992t10.271997-36.287989zM1023.99984 511.99984q0 103.999968-40.575987 198.847938t-109.151966 163.423949-163.423949 109.151966-198.847938 40.575987-198.847938-40.575987-163.423949-109.151966-109.151966-163.423949-40.575987-198.847938 40.575987-198.847938 109.151966-163.423949 163.423949-109.151966 198.847938-40.575987 198.847938 40.575987 163.423949 109.151966 109.151966 163.423949 40.575987 198.847938z"
+                      p-id="4567" data-spm-anchor-id="a313x.search_index.0.i4.73f13a81QDVP27" class="selected" :fill="useIconColor"></path>
+                  </svg>
+                  <span class="inline-block align-middle ml-1 font-medium">Gitee</span>
+            </anchor>
+          </span>
 
           <!-- Project WebSite Card -->
           <span v-if="displayWebSite" class="tip2 align-middle">
-        <anchor :href="useProject?.website" title="WebSite" target="_blank" mode="classic">
-             <home class="inline-block align-middle" theme="filled" size="14" :fill="useIconColor" />
-              <span class="inline-block align-middle ml-1 font-medium">WebSite</span>
-        </anchor>
-      </span>
+            <anchor :href="useProject?.website" title="WebSite" target="_blank" mode="classic">
+                 <home class="inline-block align-middle" theme="filled" size="14" :fill="useIconColor" />
+                  <span class="inline-block align-middle ml-1 font-medium">WebSite</span>
+            </anchor>
+          </span>
 
         </template>
 
@@ -334,7 +334,6 @@ onUnmounted(() => {
           <span v-if="displayLanguage" class="tip2 language-card">
             <Code theme="outline" size="16" :fill="useIconColor" class="inline-block align-middle" />
             <span v-for="language in useLanguageArray" :key="language" class="inline-block align-middle">
-    <!--          <img :src="`/images/languages/${language}.png`" width="14" :alt="language" :title="language" />-->
               {{ language }}
               </span>
           </span>
